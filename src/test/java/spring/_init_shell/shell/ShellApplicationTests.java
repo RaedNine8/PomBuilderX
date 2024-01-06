b0d06fec7f35b2dcf4bfced5ad58cb77e3e32a78
Raed Nefzi <raednefzi1998@gmail.com>
2024-01-06 16:26:13 +0100

    first_commit

---
+++ src/test/java/spring/_init_shell/shell/ShellApplicationTests.java
@@ -0,0 +1,13 @@
+package spring._init_shell.shell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
